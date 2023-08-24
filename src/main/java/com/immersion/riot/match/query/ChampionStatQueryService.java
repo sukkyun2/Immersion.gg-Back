de3b516d0f2b85f1801076705b76d2200f98fb7d
강지원 <kangji149@naver.com>
2023-08-24 17:22:56 +0900

    feat: 통게 가져올때 예외 처리

--- src/main/java/com/immersion/riot/match/query/ChampionStatQueryService.java
+++ src/main/java/com/immersion/riot/match/query/ChampionStatQueryService.java
@@ -1,5 +1,6 @@
 package com.immersion.riot.match.query;
 
+import com.immersion.riot.common.app.NoDataException;
 import com.immersion.riot.match.app.dto.ChampionStatResponse;
 import com.immersion.riot.match.app.dto.ChampionStatsDto;
 import com.immersion.riot.match.app.service.ImageUrlBuilderService;
@@ -7,12 +8,14 @@ import com.immersion.riot.match.domain.repository.ParticipantRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class ChampionStatQueryService {
 
     private final ParticipantRepository participantRepository;
@@ -20,11 +23,18 @@ public class ChampionStatQueryService {
 
 
     public List<ChampionStatResponse> getMostChampionByPuuid(String puuid) {
-        return participantRepository.getChampionStats(puuid).stream()
+
+        List<ChampionStatsDto> championStats = participantRepository.getChampionStats(puuid);
+
+        if (championStats.isEmpty()) {
+            throw new NoDataException("해당하는 유저의 챔피언 통계를 가져올 수 없습니다 puuid - "+ puuid);
+        }
+
+        return championStats.stream()
                 .map(dto -> ChampionStatResponse.from(
-                        dto, imageUrlBuilderService.getChampionImageUrl(dto.championId()))
-                )
-                .toList();
+                        dto,
+                        imageUrlBuilderService.getChampionImageUrl(dto.championId()))
+                ).toList();
     }
 
 }
