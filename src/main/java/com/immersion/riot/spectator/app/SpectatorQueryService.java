247b2df00b52b2576ab9bdc5f64487076ce8d447
nothingkim <hp90047@gmail.com>
2023-08-24 17:56:05 +0900

    feat: Spectator API DTO 가공

--- src/main/java/com/immersion/riot/spectator/app/SpectatorQueryService.java
+++ src/main/java/com/immersion/riot/spectator/app/SpectatorQueryService.java
@@ -2,9 +2,9 @@ package com.immersion.riot.spectator.app;
 
 import com.immersion.riot.common.app.NoDataException;
 import com.immersion.riot.spectator.infra.SpectatorClient;
+import com.immersion.riot.spectator.infra.SpectatorQueryResponse;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 @Service
 public class SpectatorQueryService {
     private final SpectatorClient spectatorClient;
@@ -13,16 +13,29 @@ public class SpectatorQueryService {
         this.spectatorClient = spectatorClient;
     }
 
-    public SpectatorQueryResponse getSpectator(String encryptedSummonerId) throws NoDataException {
+    public SpectatorDTO getSpectator(String encryptedSummonerId) throws NoDataException {
         SpectatorQueryResponse response = spectatorClient.getSpectator(encryptedSummonerId);
 
         if (response == null) {
             throw new NoDataException();
         }
 
-        SpectatorQueryResponse spectatorQueryResponse = new SpectatorQueryResponse();
+        SpectatorDTO spectatorDTO = new SpectatorDTO();
+        spectatorDTO.setGameId(response.getGameId());
+        spectatorDTO.setMapId(response.getMapId());
+        spectatorDTO.setGameMode(response.getGameMode());
+        spectatorDTO.setGameType(response.getGameType());
+        spectatorDTO.setGameQueueConfigId(response.getGameQueueConfigId());
+        for (SpectatorDTO.CurrentGameParticipant participants: spectatorDTO.getParticipants()
+             ) {
+            spectatorDTO.setParticipants(response.getParticipants());
+        }
+
+        spectatorDTO.setPlatformId(response.getPlatformId());
+        spectatorDTO.setGameStartTime(response.getGameStartTime());
+        spectatorDTO.setGameLength(response.getGameLength());
 
 
-        return response;
+        return spectatorDTO;
     }
 }
